d74274f67df001434789ab287787b2f94605629a
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-01-09 06:59:08 +0100

    Add implementations before completion

--- src/GoogleDataObjectImpl.php
+++ src/GoogleDataObjectImpl.php
@@ -19,30 +19,51 @@ class GoogleDataObjectImpl implements IDataObject
 
     public function doesExist($remoteFullPath): bool
     {
-        if (substr($remoteFullPath, -1) === '/') {
-            return $this->bucket->object($remoteFullPath)->exists();
-        } else {
+        $occurrences = substr_count($remoteFullPath, '/');
+        if ($occurrences <= 2) {
             return $this->bucket->exists();
+        } else {
+            return $this->bucket->object($remoteFullPath)->exists();
         }
     }
 
-    public function upload($bytes, $localFullPath): void
+    public function upload($localFullPath, $remoteFullPath): void
     {
-        //
+        $this->bucket->upload(
+            fopen($localFullPath, 'r'),
+            ['name' => $remoteFullPath]
+        );
     }
 
-    public function download($bytes, $localFullPath)
+    public function download($remoteFullPath, $localFullPath): void
     {
-        //
+        $this->bucket->object($remoteFullPath)->downloadToFile($localFullPath);
     }
 
     public function publish($remoteFullPath, $expirationTime = 90): string
     {
-        //
+        if (!$this->doesExist($remoteFullPath)) {
+            throw new Exception();
+        } else {
+            $object = $this->bucket->object($remoteFullPath);
+            $url = $object->signedUrl(
+                new \DateTime('+' . $expirationTime . ' minutes'),
+                ['version' => 'v4']
+            );
+            echo $url;
+            return $url;
+        }
     }
 
     public function remove($remoteFullPath, $recursive = false): void
     {
-        //
+        if ($recursive) {
+            $objects = $this->bucket->objects(['prefix' => $remoteFullPath]);
+            foreach ($objects as $object) {
+                $object->delete();
+            }
+        } else {
+            $this->bucket->object($remoteFullPath)->delete();
+        }
     }
 }
--- src/LabelDetectorImpl.php
+++ src/LabelDetectorImpl.php
@@ -1,12 +1,13 @@
 <?php
 require __DIR__ . '/ILabelDetector.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
 class LabelDetectorImpl implements ILabelDetector
 {
     private $client;
-    private $metrics;
+    private $metrics = [];
     private $amountOfLabels;
 
     public function __construct()
@@ -17,22 +18,28 @@ class LabelDetectorImpl implements ILabelDetector
         ]);
     }
 
-    public function analyze($remoteFullPath, $maxLabels = 10, $minConfidenceLevel = 0.9): array
+    public function analyze($remoteFullPath, $maxLabels = 10, $minConfidenceLevel = 90): array
     {
         $image = file_get_contents($remoteFullPath);
         $response = $this->client->labelDetection($image);
         $labels = $response->getLabelAnnotations();
 
         foreach ($labels as $label) {
-            $this->metrics = [
-                "description" => $label->getDescription(),
-                "confidenceLevel" => $label->getScore()
-            ];
+            if (count($this->metrics) >= $maxLabels) {
+                break;
+            }
+            if ($label->getScore() * 100 >= $minConfidenceLevel) {
+                $this->metrics[] = [
+                    "description" => $label->getDescription(),
+                    "confidenceLevel" => $label->getScore() * 100
+                ];
+            }
         }
+        $this->amountOfLabels = count($this->metrics);
 
         return [
-            "amountOfLabels" => $this->amountOfLabels,
-            "metrics" => $this->metrics
+            "metrics" => $this->metrics,
+            "amountOfLabels" => $this->amountOfLabels
         ];
     }
 }
