4272b6d8b204e8925ce765c160384270b482f6eb
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-01-09 22:19:05 +0100

    Clean main Sequence and Add results to SQL file uploaded to cloud storage

--- index.php
+++ index.php
@@ -2,27 +2,46 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$env = parse_ini_file('.env');
-$bucketName = $env['BUCKET_NAME'];
-$bucketUri = $env['BUCKET_URI'];
+callApi();
 
-$labelDetector = new App\LabelDetectorImpl();
-$dataObject = new App\GoogleDataObjectImpl($bucketName);
+function callApi()
+{
+    $env = parse_ini_file('.env');
+    $bucketName = $env['BUCKET_NAME'];
+    $bucketUri = $env['BUCKET_URI'];
+    $labelDetector = new App\LabelDetectorImpl();
+    $dataObject = new App\GoogleDataObjectImpl($bucketName);
 
-$objectUri = $bucketUri . '/sample.jpeg';
-$localFile = 'images/sample.jpeg';
+    $objectUri = $bucketUri . '/sample.jpeg';
+    $localFile = 'images/sample.jpeg';
+    $sqlObjectUri = $bucketUri . '/labels.sql';
+    $sqlFile = 'labels.sql';
 
-clearBucket($dataObject);
+    clearBucket($dataObject);
 
-if (!$dataObject->doesExist($objectUri)) {
-    $dataObject->upload($localFile, $objectUri);
+    if (!$dataObject->doesExist($objectUri)) {
+        $dataObject->upload($localFile, $objectUri);
+    }
+
+    $signedUrl = $dataObject->publish($objectUri);
+    $response = $labelDetector->analyze($signedUrl);
+
+    $sql = "INSERT INTO `labels` (`description`, `confidenceLevel`) VALUES ";
+    $values = [];
+    foreach ($response['metrics'] as $metric) {
+        $values[] = "('" . $metric['description'] . "', " . $metric['confidenceLevel'] . ")";
+    }
+    $sql .= implode(',', $values) . ';';
+    file_put_contents($sqlFile, $sql);
+
+    $dataObject->upload($sqlFile, $sqlObjectUri);
+
+    if ($dataObject->doesExist($sqlObjectUri)) {
+        echo 'Success!';
+    } else {
+        echo 'Failure!';
+    }
 }
-
-$signedUrl = $dataObject->publish($objectUri);
-
-$response = $labelDetector->analyze($signedUrl);
-
-echo json_encode($response);
 
 function clearBucket($dataObject)
 {
