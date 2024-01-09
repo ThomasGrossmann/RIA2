af1b93b730bb5994e6da57d68bd1a217d2abede9
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-01-09 16:43:52 +0100

    Add Sequence to test global

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
 
-require __DIR__ . '/LabelDetectorImpl.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-$labelDetector = new LabelDetectorImpl();
-$labels = $labelDetector->analyze('images/sample.jpeg');
-echo json_encode($labels);
+$env = parse_ini_file('.env');
+$bucketName = $env['BUCKET_NAME'];
+$bucketUri = $env['BUCKET_URI'];
+
+$labelDetector = new App\LabelDetectorImpl();
+$dataObject = new App\GoogleDataObjectImpl($bucketName);
+
+$objectUri = $bucketUri . '/sample.jpeg';
+$localFile = 'images/sample.jpeg';
+
+clearBucket($dataObject);
+
+if (!$dataObject->doesExist($objectUri)) {
+    $dataObject->upload($localFile, $objectUri);
+}
+
+$signedUrl = $dataObject->publish($objectUri);
+
+$response = $labelDetector->analyze($signedUrl);
+
+echo json_encode($response);
+
+function clearBucket($dataObject)
+{
+    $objectsToClear = $dataObject->bucket->objects();
+    foreach ($objectsToClear as $object) {
+        $object->delete();
+    }
+}
