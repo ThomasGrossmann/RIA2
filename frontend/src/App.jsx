a084beabc19b287080dde8e29fa33d3c2f40c86c
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2024-03-12 18:42:58 +0100

    Use MirageJS mock server to simulate API call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,27 +5,88 @@ import Dropzone from './components/Dropzone';
 import Parameters from './components/Parameters';
 import AnalyzeButton from './components/AnalyzeButton';
 import AnalyzeResults from './components/AnalyzeResults';
+import { useState } from 'react';
+import { createServer } from 'miragejs';
 
 function App() {
+  createServer({
+    routes() {
+      this.get('/api/mock', () => {
+        return {
+          metrics: [
+            {
+              description: 'Test',
+              confidenceLevel: 100
+            },
+            {
+              description: 'Test 2',
+              confidenceLevel: 98
+            },
+            {
+              description: 'Test 3',
+              confidenceLevel: 19
+            },
+            {
+              description: 'Test 4',
+              confidenceLevel: 40
+            },
+            {
+              description: 'Test 5',
+              confidenceLevel: 52
+            },
+            {
+              description: 'Test 6',
+              confidenceLevel: 99
+            },
+            {
+              description: 'Test 7',
+              confidenceLevel: 11
+            },
+            {
+              description: 'Test 8',
+              confidenceLevel: 75
+            },
+            {
+              description: 'Test 9',
+              confidenceLevel: 58
+            },
+            {
+              description: 'Test 10',
+              confidenceLevel: 100
+            }
+          ]
+        };
+      });
+    }
+  });
+  const [results, setResults] = useState([]);
+
+  function analyze(event) {
+    event.preventDefault();
+
+    fetch('/api/mock')
+      .then(response => response.json())
+      .then(data => {
+        setResults(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <>
       <header className='flex flex-row justify-end p-4 top-0 sticky'>
         <LanguageSwitcher />
       </header>
-      <main className='flex flex-col items-center mx-20 mb-10 gap-3'>
-        <section className='border border-red-400 p-12 w-full'>
+      <main className='flex flex-col items-center mx-auto w-2/3 my-10'>
+        <form id='formAnalyze' onSubmit={analyze} className='flex flex-col w-full gap-12'>
           <Dropzone />
-        </section>
-        <section className='border border-green-400 p-12 w-full'>
           <Parameters />
-        </section>
-        <section className='border border-white-400 p-12 w-full'>
           <AnalyzeButton />
-        </section>
-        <div className='border w-full opacity-50'></div>
-        <section className='border border-yellow-400 p-12 w-full'>
-          <AnalyzeResults />
-        </section>
+          <div className='border w-full opacity-50'></div>
+          <AnalyzeResults results={results} />
+        </form>
       </main>
     </>
   );
--- frontend/src/components/AnalyzeResults.jsx
+++ frontend/src/components/AnalyzeResults.jsx
@@ -1,25 +1,30 @@
 import { useTranslation } from 'react-i18next';
 import i18n from '../i18n';
 
-const AnalyzeResults = () => {
+const AnalyzeResults = ({ results }) => {
   const { t } = useTranslation();
 
   return (
     <>
-      <table className='w-full border border-slate-400'>
-        <thead className='bg-slate-400'>
-          <tr>
-            <th className='p-2'>Labels</th>
-            <th className='p-2'>Confidence</th>
-          </tr>
-        </thead>
-        <tbody className='text-center'>
-          <tr>
-            <td className='p-2'>Label 1</td>
-            <td className='p-2'>90</td>
-          </tr>
-        </tbody>
-      </table>
+      <div>
+        <span>{t("analyze_results")}</span>
+        <table id="results-table" className='w-full border border-slate-400'>
+          <thead className='bg-slate-400'>
+            <tr>
+              <th>{t("labels")}</th>
+              <th>{t("confidence")}</th>
+            </tr>
+          </thead>
+          <tbody className='text-center'>
+            {results.metrics && results.metrics.length > 0 && results.metrics.map((result, index) => (
+              <tr key={index}>
+                <td>{result.description}</td>
+                <td>{result.confidenceLevel}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
