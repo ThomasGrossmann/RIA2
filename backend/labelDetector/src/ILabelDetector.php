9edeb0dbbd59b7223fefcbb6e356b01add7a2568
Thomas Grossmann <thomas.grossmann@eduvaud.ch>
2024-03-27 19:29:47 +0100

    Delete unused old backend folder

--- backend/labelDetector/src/ILabelDetector.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\LabelDetector;
-
-interface ILabelDetector
-{
-    public function analyze(string $remoteFullPath, int $maxLabels = 10, int $minConfidenceLevel = 90): array;
-}
